e7bcc7c28ec3c17f412a158dee3735801bf584e8
OLEHFIDELIS <olehfidelis3609@icloud.com>
2024-05-23 15:18:05 +0100

    worked on the frontend login form

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+app.get("/", (req,res) => {
+    res.render()
+});
+
+app.post("/", (req,res) => {
+    req.body
+
+})
+
+
+
+app.listen("3000", () => {
+    console.log("YelpCamp Server Just Started");
+});
