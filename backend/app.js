7314b8f6e244eff77b1ab4c06f9125663eab16d9
OLEHFIDELIS <olehfidelis3609@icloud.com>
2024-05-24 21:11:39 +0100

    worked on the post request body

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,22 @@
 const express = require("express");
+const cors    = require("cors");
+const app = express();
 
-app.get("/", (req,res) => {
-    res.render()
-});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin:["http://127.0.0.1:5500/frontend/index.html"]
+}))
+
 
 app.post("/", (req,res) => {
-    req.body
+    console.log(req.body)
+    
 
 })
 
 
 
-app.listen("3000", () => {
+app.listen("4000", () => {
     console.log("YelpCamp Server Just Started");
 });
