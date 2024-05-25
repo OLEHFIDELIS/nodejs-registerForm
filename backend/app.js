e4d7df5b578ad6bf1c0a27b937e20e059c0a2edb
OLEHFIDELIS <olehfidelis3609@icloud.com>
2024-05-25 11:40:26 +0100

    worked on the mail and password validation

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors    = require("cors");
+const validator = require("email-validator");
 const app = express();
 
 
@@ -10,12 +11,23 @@ app.use(cors({
 
 
 app.post("/", (req,res) => {
-    console.log(req.body)
-    
+ const validmail =   validator.validate(req.body.email);
+ const validpass = validatePassword(req.body.password)
+    if (validmail == true && validpass == true) {
+        console.log("correct")
+    } else  {
+        console.log("error")
+    };
 
-})
+   console.log(req.body)
+   res.redirect("http://127.0.0.1:5500/frontend/index.html")
+});
 
 
+function validatePassword(password) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+};
 
 app.listen("4000", () => {
     console.log("YelpCamp Server Just Started");
